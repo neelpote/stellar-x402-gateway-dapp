b3b426a192ac8a5fb90ee158b1109e78b7b346fc
neelpote <neelpote96@gmail.com>
2026-06-29 20:17:19 +0530

    feat: enhance UI dashboard with interactive premium resource selector

--- tests/frontend.test.tsx
+++ tests/frontend.test.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import Home from "../pages/index";
 
 describe("Stellar x402 Gateway Dashboard", () => {
